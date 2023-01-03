51b9b062577b3abf068e30ece3d35b012f329a84
Lam <Lamntpd05350@fpt.edu.vn>
2023-01-03 23:31:18 +0700

    Lam

--- src/main/java/slide4/SinhVien.java
+++ src/main/java/slide4/SinhVien.java
@@ -1,9 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package slide4;
 
-import java.util.Scanner;
-
+/**
+ *
+ * @author DELL
+ */
 public class SinhVien {
-	private String ten ;
+    private String ten ;
     private Integer tuoi ;
     private String lop ;
 
@@ -29,5 +36,4 @@ public class SinhVien {
         sv1.showSinhVien();
         sv1.DoiLop("TKTW");
     }
-	
 }
